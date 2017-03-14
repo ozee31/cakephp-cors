917e8678ee771ab39bace5868cc6f1f72c90abc8
bravo-kernel <bravo-kernel@users.noreply.github.com>
2017-03-14 17:45:57 +0000

    Adds base for tests

---
+++ tests/TestCase/Controller/ErrorControllerTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Cors\TestCase\Controller;
+
+use PHPUnit\Framework\TestCase;
+
+class ErrorControllerTest extends TestCase
+{
+    public function testPlaceholder()
+    {
+        $this->markTestIncomplete('This repo could do with some tests');
+    }
+}
