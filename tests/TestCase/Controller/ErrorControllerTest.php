6eee3553da145c2bcb5e0a5dfe54c9a42fc5713f
Ozee31 <flavien.beninca@gmail.com>
2017-03-22 09:18:42 +0000

    test ErrorController

--- tests/TestCase/Controller/ErrorControllerTest.php
+++ tests/TestCase/Controller/ErrorControllerTest.php
@@ -2,11 +2,33 @@
 namespace Cors\TestCase\Controller;
 
 use PHPUnit\Framework\TestCase;
+use Cake\Network\Request;
+use Cake\Network\Response;
+use Cake\Event\Event;
 
 class ErrorControllerTest extends TestCase
 {
-    public function testPlaceholder()
+    private $controller;
+
+    public function setUp()
     {
-        $this->markTestIncomplete('This repo could do with some tests');
+        parent::setUp();
+        $request = new Request();
+        $response = new Response();
+        $this->controller = $this->getMockBuilder('Cors\Controller\ErrorController')
+            ->setConstructorArgs([$request, $response])
+            ->setMethods(null)
+            ->getMock();
+    }
+
+    public function testInitializeLoadRequestHandler() {
+        $this->assertInstanceOf('Cake\Controller\Component\RequestHandlerComponent', $this->controller->RequestHandler);
+    }
+
+    public function testBeforeRenderAllowAllOrigin() {
+        $event = new Event('Controller.startup', $this->controller);
+        $this->controller->beforeRender($event);
+        $responsesHeader = $this->controller->response->header();
+        $this->assertEquals('*', $responsesHeader['Access-Control-Allow-Origin']);
     }
 }
